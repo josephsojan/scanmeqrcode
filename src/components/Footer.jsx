72139329adfed668229ea9fa09f67eaf516fc6f5
Joseph <josephsojanofficial@gmail.com>
2026-08-25 23:25:54 +0530

    Modified Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,15 @@ export function Footer() {
     return (
         <footer>
             <div className="footer-section">
-                <span className="footer-copy">© 2026 ScanMe.</span>
+                <span className="footer-copy">JS © 2026 ScanMe.</span>
                 <span>All rights reserved.</span>
                 <span className="footer-dot">•</span>
-                <span>JS</span>
+                <span>Made with Passion</span>
             </div>
             <div className="footer-section">
                 <span> Secure and Encrypted</span>
                 <span class="footer-dot">•</span>
-                <span>High-Fidelity Rendering</span>
+                <span>JS</span>
             </div>
         </footer>
     );
